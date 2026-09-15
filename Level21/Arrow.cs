39e3c790a736752101fee5bfc338c54ae90590b8
odie-bobs <140290205+odie-bobs@users.noreply.github.com>
2026-09-15 16:18:39 -0500

    init

---
+++ Level21/Arrow.cs
@@ -0,0 +1,54 @@
+namespace Level21;
+
+public class Arrow
+{
+
+  public Head Head { get; set; }
+  public float Length { get; private set; }
+  public Fletching Fletching { get; set; }
+
+  public Arrow(Head head, float length, Fletching fletching)
+  {
+    Head = head;
+    Length = length;
+    Fletching = fletching;
+  }
+
+  public float GetCost()
+  {
+    float lengthCost = Length * 0.05f;
+
+    var headCost = Head switch
+    {
+      Head.Wood => 3,
+      Head.Obsidian => 5,
+      Head.Steel => 10,
+      _ => 0,
+    };
+
+    var fletchingCost = Fletching switch
+    {
+      Fletching.GooseFeathers => 3,
+      Fletching.TurkeyFeathers => 5,
+      Fletching.Plastic => 10,
+      _ => 0,
+    };
+    return lengthCost + headCost + fletchingCost;
+
+  }
+}
+
+public enum Head
+{
+  Steel,
+  Wood,
+  Obsidian
+}
+
+public enum Fletching
+{
+  Plastic,
+  TurkeyFeathers,
+  GooseFeathers,
+}
+
