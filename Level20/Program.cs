9a516a862b59a665f6a8cca04157f899aa38cf7e
odie-bobs <140290205+odie-bobs@users.noreply.github.com>
2026-09-15 10:17:51 -0500

    init

---
+++ Level20/Program.cs
@@ -0,0 +1,8 @@
+﻿using Level18;
+
+var arrow1 = new Arrow(Head.Obsidian, .6f, Fletching.Plastic);
+Console.WriteLine(arrow1.GetCost());
+
+
+var arrow2 = new Arrow(Head.Steel, 1f, Fletching.Plastic);
+Console.WriteLine(arrow2.GetCost());
